bb46e19a1acfcabb5c9ba6bd3fe749b5971ff7a4
JagdevMishra <50991410+JagdevMishra@users.noreply.github.com>
2022-11-15 20:21:31 +0530

    commit

---
+++ Api.DataAggregatorSvc/Helpers/BaseProxy.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Api.DataAggregatorSvc.Helpers
+{
+    public class BaseProxy<T> where T : class
+    {
+        protected readonly ILogger<T> _logger;
+        protected readonly IHttpClientFactory _httpClientFactory;
+
+        public BaseProxy(ILogger<T> logger, IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        protected HttpClient httpClient => _httpClientFactory.CreateClient("HttpClient");
+
+        protected async Task<TT> GetDataAsync<TT>(Uri requestUrl)
+        {
+            try
+            {
+                using HttpResponseMessage httpResponseMessage = await httpClient.GetAsync(requestUrl);
+                httpResponseMessage.EnsureSuccessStatusCode();
+                var response = await httpResponseMessage.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<TT>(response);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Get Error Message", requestUrl?.AbsoluteUri);
+                throw;
+            }
+        }
+        protected async Task<TT> PostDataAsync<TT>(Uri requestUrl, HttpContent content)
+        {
+            try
+            {
+                using HttpResponseMessage httpResponseMessage = await httpClient.PostAsync(requestUrl,content);
+                httpResponseMessage.EnsureSuccessStatusCode();
+                var response = await httpResponseMessage.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<TT>(response);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Post Error Message", requestUrl?.AbsoluteUri);
+                throw;
+            }
+        }
+    }
+}
